ec4480c5d834e6104568fb5d719066bf755b33f7
Szymon Sala <zawsze.pod.wszystkim@gmail.com>
2022-01-14 23:47:50 +0100

    add PostgreSQL database

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -83,8 +83,19 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'eats_db',
+
+        'USER': 'admin',
+
+        'PASSWORD': 'admin',
+
+        'HOST': '192.168.100.12',
+
+        'PORT': '5432',
+
     }
 }
 
