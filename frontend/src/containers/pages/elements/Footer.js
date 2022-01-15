4ef85b1f91394f2e0b53e45c905a7c43883ec9b0
Przemysław Sałek <psalek1999@gmail.com>
2022-01-15 12:13:19 +0100

    create custom page

---
+++ frontend/src/containers/pages/elements/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import "./Footer.css";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+        <p>PWP Project 2022</p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
