2ded8a308a53ff0821f80e8520d1dac8c7f73586
Przemysław Sałek <psalek1999@gmail.com>
2022-01-23 14:23:05 +0100

    web title and icon

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -11,8 +11,26 @@ export default function () {
     <Router>
       <Routes>
         <Route path="/" element={<CustomPage mainPage={BoxTileMenu} />} />
-        <Route path="/todo" element={<CustomPage mainPage={ToDo} />} />
-        <Route path="/login" element={<CustomPage mainPage={BoxAuth} />} />
+        <Route
+          path="/todo"
+          element={<CustomPage title="To-do list" mainPage={ToDo} />}
+        />
+        <Route
+          path="/login"
+          element={<CustomPage title="Logowanie" mainPage={BoxAuth} />}
+        />
+        <Route
+          path="/food-order"
+          element={
+            <CustomPage title="Zamów jedzenie" mainPage={BoxFoodOrder} />
+          }
+        />
+        <Route
+          path="/orders/history"
+          element={
+            <CustomPage title="Historia zamówień" mainPage={BoxOrdersHistory} />
+          }
+        />
       </Routes>
     </Router>
   );
--- frontend/src/containers/pages/CustomPage.js
+++ frontend/src/containers/pages/CustomPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+
 import Footer from "./elements/Footer";
 import Header from "./elements/Header";
 import Main from "./elements/Main";
@@ -6,6 +7,24 @@ import Main from "./elements/Main";
 import "./CustomPage.css";
 
 export default class CustomPage extends Component {
+  componentDidMount() {
+    if (this.props.title) {
+      document.title = "9eats - " + this.props.title;
+    } else {
+      document.title = "9eats - Zamów i zapłać on-line";
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.title !== this.props.title) {
+      if (this.props.title) {
+        document.title = "9eats - " + this.props.title;
+      } else {
+        document.title = "9eats - Zamów i zapłać on-line";
+      }
+    }
+  }
+
   render() {
     const { mainPage } = this.props;
 
